3fe6d24e40311e9a53d426d6db8fc0456c62782c
Vincent Ollivier <v@vinc.cc>
2019-12-13 09:09:40 +0100

    Add colors to day 13

--- ruby/2019/13/aoc2.rb
+++ ruby/2019/13/aoc2.rb
@@ -1,15 +1,25 @@
 require "tty-prompt"
+require "rainbow/refinement"
 
 require_relative "./computer.rb"
+
+using Rainbow
 
 def tile(code)
   case code
     when 0 then " " # empty
     when 1 then "#" # wall
     when 2 then "X" # block
-    when 3 then "-" # paddle
-    when 4 then "o" # ball
+    when 3 then "=".bold.red # paddle
+    when 4 then "o".bold.red # ball
   end
+end
+
+def rainbow(freq, i)
+   r = Math.sin(freq * i + 0) * 127 + 128
+   g = Math.sin(freq * i + 2 * Math::PI / 3) * 127 + 128
+   b = Math.sin(freq * i + 4 * Math::PI / 3) * 127 + 128
+   format("#%02X%02X%02X", r, g, b)
 end
 
 screen = Array.new(23).map { Array.new }
@@ -42,9 +52,13 @@ until computer.halted?
 
       puts "\e[H\e[2J"
       puts screen[0].join
-      puts format("%s %s %s", tile(1), score.to_s.center(screen[0].size - 4), tile(1))
-      screen.each do |line|
-        puts line.join
+      puts format("%s %s %s", tile(1), score.to_s.center(screen[0].size - 4).bold.red, tile(1))
+      screen.each_with_index do |line, i|
+        if 1 < i && i < 17
+          puts line.join.gsub("X", "X".color(rainbow(0.3, i)))
+        else
+          puts line.join
+        end
       end
     end
 
