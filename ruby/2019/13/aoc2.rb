ea9008942ef544eca5402a25adb38427fda81582
Vincent Ollivier <v@vinc.cc>
2019-12-13 09:37:54 +0100

    Refactor gameplay

--- ruby/2019/13/aoc2.rb
+++ ruby/2019/13/aoc2.rb
@@ -5,6 +5,13 @@ require_relative "./computer.rb"
 
 using Rainbow
 
+def rainbow(freq, i)
+   r = Math.sin(freq * i + 0) * 127 + 128
+   g = Math.sin(freq * i + 2 * Math::PI / 3) * 127 + 128
+   b = Math.sin(freq * i + 4 * Math::PI / 3) * 127 + 128
+   format("#%02X%02X%02X", r, g, b)
+end
+
 def tile(code)
   case code
     when 0 then " " # empty
@@ -15,69 +22,70 @@ def tile(code)
   end
 end
 
-def rainbow(freq, i)
-   r = Math.sin(freq * i + 0) * 127 + 128
-   g = Math.sin(freq * i + 2 * Math::PI / 3) * 127 + 128
-   b = Math.sin(freq * i + 4 * Math::PI / 3) * 127 + 128
-   format("#%02X%02X%02X", r, g, b)
-end
-
-screen = Array.new(23).map { Array.new }
 prompt = TTY::Prompt.new
 
-program = File.read(File.join(__dir__, "input")).split(",").map(&:to_i)
-program[0] = 2 # Play for free
-computer = Computer.new(program.dup)
-# computer.debug = true
-autoplay = true
-until computer.halted?
-  computer.run do |outputs|
-    if outputs
-      score = 0
-      xb = 0
-      xp = 0
-
-      outputs.each_slice(3) do |x, y, t|
-        if x == -1 && y == 0
-          score = t
-        else
-          screen[y][x] = tile(t)
-
-          case t
-            when 3 then xp = x
-            when 4 then xb = x
+autoplay = ARGV.shift == "autoplay"
+speed = (ARGV.shift || "0.4").to_f
+score = 0
+loop do
+  screen = Array.new(23).map { Array.new }
+  program = File.read(File.join(__dir__, "input")).split(",").map(&:to_i)
+  program[0] = 2 # Play for free
+  computer = Computer.new(program.dup)
+  # computer.debug = true
+  until computer.halted?
+    computer.run do |outputs|
+      if outputs
+        xb = 0
+        xp = 0
+        outputs.each_slice(3) do |x, y, t|
+          if x == -1 && y == 0
+            score = t
+          else
+            screen[y][x] = tile(t)
+            case t
+              when 3 then xp = x
+              when 4 then xb = x
+            end
           end
         end
-      end
 
-      puts "\e[H\e[2J"
-      puts screen[0].join
-      puts format("%s %s %s", tile(1), score.to_s.center(screen[0].size - 4).bold.red, tile(1))
-      screen.each_with_index do |line, i|
-        if 1 < i && i < 17
-          puts line.join.gsub("X", "X".color(rainbow(0.3, i)))
-        else
-          puts line.join
+        term = "\e[H\e[2J"
+        term += screen[0].join + "\n"
+        term += format("%s %s %s\n", tile(1), score.to_s.center(screen[0].size - 4).bold.red, tile(1))
+        term += screen.each_with_index.reduce("") do |r, (l, i)|
+          if 1 < i && i < 17
+            r + l.join.gsub("X", "X".color(rainbow(0.3, i))) + "\n"
+          else
+            r + l.join + "\n"
+          end
         end
+        puts term
       end
-    end
 
-    if autoplay
-      sleep 0.1
-      if xp > xb
-        -1
-      elsif xp == xb
-        0
-      else
-        1
-      end
-    else
-      case prompt.keypress
-        when "\e[D" then -1
-        when " "    then 0
-        when "\e[C" then 1
+      if autoplay
+        sleep speed
+        if xp > xb
+          -1
+        elsif xp == xb
+          0
         else
+          1
+        end
+      else
+        #start = Time.now.to_f
+        case prompt.keypress(timeout: speed)
+          when "\e[D" then -1
+          when "\e[C" then 1
+          else 0
+        end
+        #duration = Time.now.to_f - start
+        #sleep [speed - duration, 0].max
       end
     end
+  end
+  case prompt.keypress("Play again? [Y/n]")
+    when "\r", "y" then next
+    else break
   end
 end
