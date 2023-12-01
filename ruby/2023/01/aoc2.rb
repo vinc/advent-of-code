4d8d7b7fb8a72bba531fc2d3c03a26e6edd9fe95
Vincent Ollivier <v@vinc.cc>
2023-12-01 16:23:13 +0100

    Refactor code

--- ruby/2023/01/aoc2.rb
+++ ruby/2023/01/aoc2.rb
@@ -1,8 +1,8 @@
-def decode(s)
-  %w[one 1 two 2 three 3 four 4 five 5 six 6 seven 7 eight 8 nine 9].each_slice(2) do |a, b|
-    s.gsub!(a, [a, b, a].join)
-  end
-  s
+DIGITS = %w[one 1 two 2 three 3 four 4 five 5 six 6 seven 7 eight 8 nine 9]
+
+def sub(str)
+  DIGITS.each_slice(2) { |a, b| str.gsub!(a, [a, b, a].join) }
+  str
 end
 
-puts ARGF.read.split.map { |l| decode(l).scan(/\d/).values_at(0, -1).join.to_i }.sum
+puts ARGF.read.split.map { |l| sub(l).scan(/\d/).values_at(0, -1).join.to_i }.sum
