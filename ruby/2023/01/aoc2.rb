746227f9d9ffe5026169d1e5f93f613706ce3fc1
Vincent Ollivier <v@vinc.cc>
2023-12-01 16:29:09 +0100

    Add rubocop config

--- ruby/2023/01/aoc2.rb
+++ ruby/2023/01/aoc2.rb
@@ -1,4 +1,4 @@
-DIGITS = %w[one 1 two 2 three 3 four 4 five 5 six 6 seven 7 eight 8 nine 9]
+DIGITS = %w[one 1 two 2 three 3 four 4 five 5 six 6 seven 7 eight 8 nine 9].freeze
 
 def sub(str)
   DIGITS.each_slice(2) { |a, b| str.gsub!(a, [a, b, a].join) }
