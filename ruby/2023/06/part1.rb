d7af1428b16fe02b81e02e7a3c430162a39f6ae1
Vincent Ollivier <v@vinc.cc>
2023-12-06 08:44:33 +0100

    Code golf day 6

--- ruby/2023/06/part1.rb
+++ ruby/2023/06/part1.rb
@@ -1,7 +1,2 @@
 ts, ds = ARGF.read.lines.map { |s| s.split(":").last.split.map(&:to_i) }
-games = ts.zip(ds).map do |time, dist|
-  (0..time).filter do |hold|
-    (time - hold) * hold > dist
-  end.count
-end
-p games.flatten.reduce(&:*)
+p ts.zip(ds).map { |t, d| (0..t).count { |h| (t - h) * h > d } }.reduce(&:*)
--- ruby/2023/06/part2.rb
+++ ruby/2023/06/part2.rb
@@ -1,7 +1,2 @@
-ts, ds = ARGF.read.lines.map { |s| s.gsub(/\s+/, "").split(":").last.split.map(&:to_i) }
-games = ts.zip(ds).map do |time, dist|
-  (0..time).filter do |hold|
-    (time - hold) * hold > dist
-  end.count
-end
-p games.flatten.reduce(&:*)
+t, d = ARGF.read.lines.map { |s| s.gsub(/\s+/, "").split(":").last.to_i }
+p (0..t).count { |h| (t - h) * h > d }
