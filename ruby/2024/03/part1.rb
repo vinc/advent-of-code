26a84470a88683bfd22fd83bd6598f74ef19f73a
Vincent Ollivier <v@vinc.cc>
2024-12-03 10:19:37 +0100

    Refactor part1

--- ruby/2024/03/part1.rb
+++ ruby/2024/03/part1.rb
@@ -1,1 +1,4 @@
-puts ARGF.read.scan(/mul\((\d+),(\d+)\)/).map { |l| l.map(&:to_i).reduce(&:*) }.sum
+puts ARGF.read
+         .scan(/mul\((\d+),(\d+)\)/)
+         .map { |x, y| x.to_i * y.to_i }
+         .sum
